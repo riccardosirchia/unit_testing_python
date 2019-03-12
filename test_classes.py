01a499ebd4f9ebbd9e4d9f1d3efabc39f9297165
sirchia <sirchia.riccardo@gmail.com>
2019-03-12 19:18:53 +0200

    Cleaned code and added comments to conform to PEP8

--- test_classes.py
+++ test_classes.py
@@ -2,7 +2,7 @@ from survey import AnnonymouseSurvey
 import unittest
 
 class TestAnonmousSurvey(unittest.TestCase):
-
+	'''test to check function works as expected'''
 	def setUp(self):
 		question = "What languages do you know?"
 		self.my_survey = AnnonymouseSurvey(question)
@@ -10,12 +10,12 @@ class TestAnonmousSurvey(unittest.TestCase):
 
 
 	def test_store_single_response(self):
+		'''test for a single response'''
 		self.my_survey.store_response(self.responses[0])
-
 		self.assertIn(self.responses[0], self.my_survey.responses)
 
 	def test_store_multiple_responses(self):
-
+		'''test for multiple responses'''
 		for response in self.responses:
 			self.my_survey.store_response(response)
 
--- test_name_function.py
+++ test_name_function.py
@@ -5,10 +5,12 @@ class NameTestClass (unittest.TestCase):
 	'''tests for name_function.py'''
 
 	def test_first_last_name(self):
+		'''test for just a firs and last name '''
 		formatted_name = get_formated_name('jannis', 'botha')
 		self.assertEqual(formatted_name, 'Jannis Botha')
 
 	def test_first_middle_last_name(self):
+		''' test to see if a full name with middle name works '''
 		formatted_name = get_formated_name('james', 'hunt', 'william')
 		self.assertEqual(formatted_name, 'James William Hunt')
 
